30eb7a0e74947116ea7d3d05f5eebbcb032d0bcf
anisa6121 <you@example.com>
2022-08-23 19:44:06 +0600

    style change

--- js/index.js
+++ js/index.js
@@ -11,12 +11,11 @@ function displayName(playerName) {
 		}
 		console.log(playerName);
 
-		const namelist = document.createElement("li");
-		namelist.innerHTML = `
-    <ol class="list-group  list-group-numbered ">
-        <li class="list-group-item list-group-item-secondary ">${playerName}</li>
-    </ol>
-    `;
+	const namelist = document.createElement("li");
+	namelist.className = "list-group-item list-group-item-dark";
+	namelist.innerHTML = `
+     ${playerName} 
+     `;
 
 		playerList.appendChild(namelist);
 	}
@@ -37,46 +36,39 @@ function addToPlayer(element) {
 	displayName(playerArray);
 }
 
-  function disable(button) {
-		button.disabled = true;
+function disable(button) {
+	button.disabled = true;
 }
-  
 
-  function getElementById(elementId) {
-		const element = document.getElementById(elementId);
-		const elementValueString = element.innerText;
-		const value = parseFloat(elementValueString);
-		return value;
+function getElementById(elementId) {
+	const element = document.getElementById(elementId);
+	const elementValueString = element.innerText;
+	const value = parseFloat(elementValueString);
+	return value;
 }
-  
 
-  document
-		.getElementById("player-expense-btn")
-		.addEventListener("click", function () {
-			const playerExpense =
-				document.getElementById("expense-amount");
+document.getElementById("player-expense-btn").addEventListener("click", function () {
+	const playerExpense = document.getElementById("expense-amount");
 
-			const perPlayerCost = getElementById("per-player-cost");
+	const perPlayerCost = getElementById("per-player-cost");
 
-			const totalExpense = perPlayerCost * playerArray.length;
+	const totalExpense = perPlayerCost * playerArray.length;
 
-			playerExpense.innerText = totalExpense;
-		});
+	playerExpense.innerText = totalExpense;
+	});
 
-  document
-		.getElementById("total-cost-amount-btn")
-		.addEventListener("click", function () {
-			const totalAmountCost =
-				document.getElementById("total-cost");
+document.getElementById("total-cost-amount-btn").addEventListener("click", function () {
+    
+      const totalAmountCost = document.getElementById("total-cost");
 
-			const playerExpense = getElementById("expense-amount");
+		const playerExpense = getElementById("expense-amount");
 
-			const ManagerExpense = getElementById("manager-cost");
+		const ManagerExpense = getElementById("manager-cost");
 
-			const newCoachExpense = getElementById("coach-cost");
+		const newCoachExpense = getElementById("coach-cost");
 
-			const totalPlayerExpense =
-				playerExpense + ManagerExpense + newCoachExpense;
+		const totalPlayerExpense =
+			playerExpense + ManagerExpense + newCoachExpense;
 
-			totalAmountCost.innerText = totalPlayerExpense;
-		});
+		totalAmountCost.innerText = totalPlayerExpense;
+	});
